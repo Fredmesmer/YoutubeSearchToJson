638b9cc1189690beecdf1a10a3e330d5377a552c
Fredmesmer <49597918+Fredmesmer@users.noreply.github.com>
2021-09-29 09:40:26 +0200

    Fixed Artist name bug and added argument gathering version

--- ytapi-argathered.js
+++ ytapi-argathered.js
@@ -7,8 +7,14 @@ const puppeteer = require('puppeteer');
     await page.setExtraHTTPHeaders({
       'Accept-Language': 'en-US,en;q=0.9'
     });
+    let search_term = process.argv[2];
+    let i;
+    for(i=3;i<process.argv.length;i++){
+      search_term = search_term.concat(' ', process.argv[i]);
+    }
+    console.log("Searching for : ",search_term);
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
-    await page.goto('https://www.youtube.com/results?search_query='+process.argv[2]);
+    await page.goto('https://www.youtube.com/results?search_query='+search_term);
     const [button] = await page.$x('/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[2]/div[2]/div[5]/div[2]/ytd-button-renderer[2]/a/tp-yt-paper-button/yt-formatted-string');
     if (button) {
         await button.click();
@@ -20,7 +26,6 @@ const puppeteer = require('puppeteer');
     let title;
     let artists;
     let imglien;
-    let i;
     let selected;
 
     selected = false
@@ -49,17 +54,18 @@ const puppeteer = require('puppeteer');
     }
 
 
+
     let imaj = await page.$$('img');
     await imaj[2].getProperty('src').then(data => {imglien = data['_remoteObject']['value'];});
 
-    console.log(lien);
-    console.log(title);
-    console.log(artists);
-    console.log(imglien);
+    console.log("lien : ",lien);
+    console.log("Titre : ",title);
+    console.log("Artiste : ",artists);
+    console.log("Miniature : ",imglien);
 
 
     let file = { "lien" : lien, "titre" : title, "artiste" : artists,"image" : imglien}
     var json = JSON.stringify(file);
-    fs.writeFile('output.json',json,(err)=> {if(err){console.log(err);}else{console.log('Outputedd');}});
+    fs.writeFile('output.json',json,(err)=> {if(err){console.log(err);}else{console.log('Outputed at output.json');}});
     await browser.close();
   })();
